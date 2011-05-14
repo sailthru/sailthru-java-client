339e251e5782d19f66fc692b4ba685e690cef50a
Prajwal Tuladhar <praj@infynyxx.com>
2011-05-14 17:03:12 -0400

    ApiException to handle API related exceptions

---
+++ src/main/com/sailthru/client/exceptions/ApiException.java
@@ -0,0 +1,37 @@
+package com.sailthru.client.exceptions;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+
+/**
+ * Handle API related Exceptions
+ * @author Prajwal Tuladhar
+ */
+public class ApiException extends IOException {
+
+    private static Logger logger = Logger.getLogger(ApiException.class.getName());
+
+    public ApiException(int statusCode, String reason) {
+        super(reason);
+        logger.info(String.format("%d: %s", statusCode, reason));
+    }
+
+    public static ApiException create(HttpEntity entity, StatusLine statusLine) {
+        int statusCode = statusLine.getStatusCode();
+        String reason = "";
+        try {
+            InputStream inputStream = entity.getContent();
+            DataInputStream dis = new DataInputStream(inputStream);
+            reason = dis.readUTF();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (IllegalStateException ex) {
+            ex.printStackTrace();
+        }
+        return new ApiException(statusCode, reason);
+    }
+}
