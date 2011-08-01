0d54844cec64d50ad78d5119681e84cf1ee74441
Prajwal Tuladhar <praj@infynyxx.com>
2011-08-01 02:06:14 -0400

    intial tests

---
+++ src/test/com/sailthru/client/SailthruUtilTest.java
@@ -0,0 +1,31 @@
+package com.sailthru.client;
+
+import java.util.ArrayList;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author praj
+ */
+public class SailthruUtilTest extends TestCase {
+    public void testMd5() {
+        String plainText1 = "hello_world";
+        String hash1 = SailthruUtil.md5(plainText1);
+        String expectedHash1 = "99b1ff8f11781541f7f89f9bd41c4a17";
+        assertEquals(hash1, expectedHash1);
+    }
+    
+    public void testArrayListToCSV() {
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("windows");
+        list1.add("linux");
+        list1.add("BSD");
+        String expectedList1 = "windows,linux,BSD";
+        assertEquals(expectedList1, SailthruUtil.arrayListToCSV(list1));
+        
+        ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("one_item");
+        String expectedList2 = "one_item";
+        assertEquals(expectedList2, SailthruUtil.arrayListToCSV(list2));
+    }
+}
