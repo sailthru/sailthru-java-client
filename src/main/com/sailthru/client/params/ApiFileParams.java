1407766e776a19fbe92d58026de3234532cc9b57
Prajwal Tuladhar <praj@infynyxx.com>
2011-08-06 22:19:57 -0400

    ApiFileParams Interface for dealing with File uploads

---
+++ src/main/com/sailthru/client/params/ApiFileParams.java
@@ -0,0 +1,12 @@
+package com.sailthru.client.params;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ *
+ * @author praj
+ */
+public interface ApiFileParams {
+    public Map<String, File> getFileParams();
+}
