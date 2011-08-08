d2ab482451a5f89b569625d49ee769def96c852d
Prajwal Tuladhar <praj@infynyxx.com>
2011-08-08 00:31:09 -0400

    ApiAction enum

---
+++ src/main/com/sailthru/client/ApiAction.java
@@ -0,0 +1,21 @@
+package com.sailthru.client;
+
+/**
+ * API calls
+ * @author praj
+ */
+public enum ApiAction {
+    email,
+    send,
+    blast,
+    template,
+    list,
+    content,
+    alert,
+    stats,
+    purchase,
+    horizon,
+    job,
+    trigger,
+    inbox
+}
