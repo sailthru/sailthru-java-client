0c481043e660dc603b903c2885c49adadce371b8
Prajwal Tuladhar <praj@infynyxx.com>
2012-02-10 02:26:12 -0500

    use final

--- src/main/com/sailthru/client/exceptions/ApiException.java
+++ src/main/com/sailthru/client/exceptions/ApiException.java
@@ -1,10 +1,6 @@
 package com.sailthru.client.exceptions;
 
-import java.io.DataInputStream;
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 import org.apache.http.StatusLine;
@@ -15,14 +11,14 @@ import org.apache.http.StatusLine;
  */
 public class ApiException extends IOException {
 
-    private static Logger logger = Logger.getLogger(ApiException.class.getName());
+    private static final Logger logger = Logger.getLogger(ApiException.class.getName());
 
     private Map<String, Object> jsonResponse;
     private int statusCode;
 
     public ApiException(int statusCode, String reason, Object jsonResponse) {
         super(reason);
-        logger.info(String.format("%d: %s", statusCode, reason));
+        logger.warning(String.format("%d: %s", statusCode, reason));
         this.jsonResponse = (Map<String, Object>)jsonResponse;
         this.statusCode = statusCode;
     }
