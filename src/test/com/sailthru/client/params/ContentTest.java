33eca3290a0d0cd4f45146aded01d6e49a1c80f7
kanaabe <kanakoabe5@gmail.com>
2013-11-30 00:47:40 -0500

    test

--- src/test/com/sailthru/client/params/ContentTest.java
+++ src/test/com/sailthru/client/params/ContentTest.java
@@ -12,18 +12,66 @@ public class ContentTest extends TestCase {
 
     public void testGetContent() {
         Content content = new Content();
-        Date date = new Date('1385553600');
+        Date date = new Date(1380831494000L);
         content.setDate(date);
-        content.setTitle('testGetContent Title');
-        content.setUrl('http://sailthru.com');
+        content.setTitle("testGetContent Title");
+        content.setUrl("http://sailthru.com");
         Map<String, Object> vars = new HashMap<String, Object>();
         vars.put("baz", "foo");
         content.setVars(vars);
-        String expected = "{\"date\" : new Date(\'Wed, 27 Nov 2013 12:00:00 GMT\'),\"tags\" : [\"baz\":\"foo\"],\"title\" : \"testGetContent Title\",\"url\" : \"http://sailthru.com\"}";
+        String expected = "{\"title\":\"testGetContent Title\",\"url\":\"http://sailthru.com\",\"date\":\"Thu Oct 03 16:18:14 EDT 2013\",\"vars\":{\"baz\":\"foo\"}}";
         String result = gson.toJson(content);
         assertEquals(expected, result);
     }
 
+    public void testSetTitle(){
+        Content content = new Content();
+        String title = "Test Title";
+        content.setTitle(title);
+        String expected = "{\"title\":\"Test Title\"}";
+        String result = gson.toJson(content);
+        assertEquals(expected,result);
+    }
+
+
+    public void testSetUrl(){
+        Content content = new Content();
+        String url = "http://sailthru.com";
+        content.setUrl(url);
+        String expected = "{\"url\":\"http://sailthru.com\"}";
+        String result = gson.toJson(content);
+        assertEquals(expected,result);
+    }
+
+    public void testSetDateFormat(){
+        Content content = new Content();
+        Date date = new Date(1380831494000L);
+        content.setDate(date);
+        String expected = "{\"date\":\"Thu Oct 03 16:18:14 EDT 2013\"}";
+        String result = gson.toJson(content);
+        assertEquals(expected,result);
+    }
+
+    public void testSetDateString(){
+        Content content = new Content();
+        String date = "1380831494000L";
+        content.setDate(date);
+        String expected = "{\"date\":\"1380831494000L\"}";
+        String result = gson.toJson(content);
+        assertEquals(expected,result);
+    }
+
+    public void testSetVars(){
+        Content content = new Content();
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("test","result");
+        vars.put("test2","result2");
+        vars.put("test3","result3");
+        content.setVars(vars);
+        String expected = "{\"vars\":{\"test\":\"result\",\"test2\":\"result2\",\"test3\":\"result3\"}}";
+        String result = gson.toJson(content);
+        assertEquals(expected,result);
+    }
 
 
 }
