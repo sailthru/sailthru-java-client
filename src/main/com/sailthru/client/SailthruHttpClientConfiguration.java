1863f6314c0b6479747ec7dd78589e8360c478f9
Prajwal Tuladhar <praj@infynyxx.com>
2013-12-04 15:44:27 -0500

    add java doc

--- src/main/com/sailthru/client/SailthruHttpClientConfiguration.java
+++ src/main/com/sailthru/client/SailthruHttpClientConfiguration.java
@@ -4,8 +4,27 @@ package com.sailthru.client;
  * Interface for providing different options to customize HTTP specific flags
  */
 public interface SailthruHttpClientConfiguration {
+    /**
+     * get connection timeout in milli seconds
+     * @return
+     */
     int getConnectionTimeout();
+
+    /**
+     * get socket timeout in milli seconds
+     * @return
+     */
     int getSoTimeout();
+
+    /**
+     * get socket reuse address boolean flag
+     * @return
+     */
     boolean getSoReuseaddr();
+
+    /**
+     * get tcp no delay boolean flag
+     * @return
+     */
     boolean getTcpNoDelay();
 }
