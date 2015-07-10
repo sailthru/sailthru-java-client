e4e790f2a9deb46a7fed462b656e461afc8893c1
kgorthi <kgorthi@bloomberg.net>
2015-07-10 17:22:13 -0400

    * Added new api end-points: GetAllLists * Removed trailing white-spaces * Imports are ordered * Deprecated methods are explicitly annotated at method level

--- src/main/com/sailthru/client/params/List.java
+++ src/main/com/sailthru/client/params/List.java
@@ -1,22 +1,23 @@
 package com.sailthru.client.params;
 
-import com.sailthru.client.ApiAction;
-import com.google.gson.reflect.TypeToken;
-import com.sailthru.client.params.query.Query;
-
 import java.lang.reflect.Type;
-import java.util.ArrayList;
+
+import com.google.gson.reflect.TypeToken;
+import com.sailthru.client.ApiAction;
+import com.sailthru.client.params.query.Query;
 
 /**
  *
  * @author Prajwal Tuladhar <praj@sailthru.com>
  */
-public class List extends AbstractApiParams implements ApiParams {
+public class List extends AbstractApiParams implements ApiParams { 
+    public static final String PARAM_LIST_ID = "list_id";
+
     protected String list;
     protected Integer primary;
     protected ListType type;
     protected Query query;
-    
+
     public List setQuery(Query query) {
         this.query = query;
         return this;
@@ -31,16 +32,18 @@ public class List extends AbstractApiParams implements ApiParams {
         this.primary = 1;
         return this;
     }
-    
+
     public List setListType(ListType listType) {
         this.type = listType;
         return this;
     }
 
+    @Override
     public ApiAction getApiCall() {
         return ApiAction.list;
     }
 
+    @Override
     public Type getType() {
         return new TypeToken<List>() {}.getType();
     }
