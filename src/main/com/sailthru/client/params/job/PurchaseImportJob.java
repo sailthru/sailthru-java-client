7b82887517ddb1c8e3f999d1cb4fee08a8e0e72c
Matt Dudys <mdudys@sailthru.com>
2020-07-11 15:04:08 -0400

    Add purchase_import job
    For importing historical purchase data.
    https://getstarted.sailthru.com/developers/api/job/#purchase-import
    
    Works similarly to Import and Update jobs which POST files.

---
+++ src/main/com/sailthru/client/params/job/PurchaseImportJob.java
@@ -0,0 +1,41 @@
+package com.sailthru.client.params.job;
+
+import com.google.gson.reflect.TypeToken;
+import com.sailthru.client.params.ApiFileParams;
+
+import java.io.File;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PurchaseImportJob extends Job implements ApiFileParams {
+
+    protected transient File file = null;
+
+    public PurchaseImportJob() {
+        this.job = "purchase_import";
+    }
+
+    public PurchaseImportJob setFile(String filePath) {
+        this.file = new File(filePath);
+        return this;
+    }
+
+    public PurchaseImportJob setFile(File file) {
+        this.file = file;
+        return this;
+    }
+
+    @Override
+    public Type getType() {
+        return new TypeToken<PurchaseImportJob>() {}.getType();
+    }
+
+    public Map<String, File> getFileParams() {
+        Map<String, File> files = new HashMap<String, File>();
+        if (this.file != null) {
+            files.put("file", this.file);
+        }
+        return files;
+    }
+}
