6ec3410949c1e31affde68e356737ab85c5fa202
Harman Singh <hsingh@sailthru.com>
2021-05-18 09:42:42 +1200

    [PT-694] Do not throw exception for successful http codes and remove redundant cases

--- src/main/com/sailthru/client/http/SailthruHandler.java
+++ src/main/com/sailthru/client/http/SailthruHandler.java
@@ -2,24 +2,19 @@ package com.sailthru.client.http;
 
 import com.sailthru.client.LastRateLimitInfo;
 import com.sailthru.client.exceptions.ApiException;
-import com.sailthru.client.exceptions.ResourceNotFoundException;
-import com.sailthru.client.exceptions.UnAuthorizedException;
 import com.sailthru.client.handler.SailthruResponseHandler;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
-
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
  * @author Prajwal Tuladhar <praj@sailthru.com>
  */
 public class SailthruHandler implements ResponseHandler<Object> {
@@ -31,58 +26,26 @@ public class SailthruHandler implements ResponseHandler<Object> {
     // key used to store rate limit info, for use and removal by the parent SailthruClient
     public static final String RATE_LIMIT_INFO_KEY = "x_rate_limit_info";
 
-    /* Supported HTTP Status codes */
-    public static final int STATUS_OK = 200;
-    public static final int STATUS_BAD_REQUEST = 400;
-    public static final int STATUS_UNAUTHORIZED = 401;
-    public static final int STATUS_FORBIDDEN = 403;
-    public static final int STATUS_NOT_FOUND = 404;
-    public static final int STATUS_METHOD_NOT_FOUND = 405;
-    public static final int STATUS_INTERNAL_SERVER_ERROR = 500;
-
     public SailthruHandler(SailthruResponseHandler handler) {
         super();
         this.handler = handler;
     }
 
-    public Object handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
-        logger.debug("Received Response: {}", httpResponse.toString());
+    public Object handleResponse(HttpResponse httpResponse) throws IOException {
+        logger.debug("Received Response: {}", httpResponse);
 
         StatusLine statusLine = httpResponse.getStatusLine();
-        int statusCode = statusLine.getStatusCode();
 
-        String jsonString = null;
-        jsonString = EntityUtils.toString(httpResponse.getEntity());
-        Object parseObject =  handler.parseResponse(jsonString);
+        String jsonString = EntityUtils.toString(httpResponse.getEntity());
+        Object parseObject = handler.parseResponse(jsonString);
 
         addRateLimitInfoToResponseObject(httpResponse, parseObject);
 
-        switch (statusCode) {
-            case STATUS_OK:
-                break;
-
-            case STATUS_BAD_REQUEST:
-                throw ApiException.create(statusLine, parseObject);
-
-            case STATUS_UNAUTHORIZED:
-                throw UnAuthorizedException.create(statusLine, parseObject);
-
-            case STATUS_FORBIDDEN:
-                throw ApiException.create(statusLine, parseObject);
-
-            case STATUS_NOT_FOUND:
-                throw ResourceNotFoundException.create(statusLine, parseObject);
-
-            case STATUS_METHOD_NOT_FOUND:
-                throw ApiException.create(statusLine, parseObject);
-
-            case STATUS_INTERNAL_SERVER_ERROR:
-                throw ApiException.create(statusLine, parseObject);
-
-            default:
-                throw ApiException.create(statusLine, parseObject);
+        // check if status is not successful (i.e. 2xx)
+        int statusCode = statusLine.getStatusCode();
+        if (statusCode / 100 != 2) {
+            throw ApiException.create(statusLine, parseObject);
         }
-        
         return parseObject;
     }
 
