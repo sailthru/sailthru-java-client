9238e5897886b7159dd6e1ae71614c9a403d0062
Adi Sivasankaran <asivasankaran@sailthru.com>
2022-01-10 21:42:02 -0500

    SRE-9014: Modify ApiFileParams to use FileInputStream

--- src/main/com/sailthru/client/params/ApiFileParams.java
+++ src/main/com/sailthru/client/params/ApiFileParams.java
@@ -1,12 +1,11 @@
 package com.sailthru.client.params;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Map;
 
-/**
- *
-* @author Prajwal Tuladhar <praj@sailthru.com>
- */
 public interface ApiFileParams {
-    public Map<String, File> getFileParams();
+    public Map<String, FileInputStream> getFileParams();
 }
